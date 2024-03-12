3ef5f80df49ef90868338b95a597c8edcbc8114f
francis mwaniki <francis@francis.franc>
2024-03-12 23:34:07 +0300

    very new changes

--- app/api/auth/counselor/requests/accept/route.ts
+++ app/api/auth/counselor/requests/accept/route.ts
@@ -1,13 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+
+import nodemailer from 'nodemailer';
 const prisma = new PrismaClient();
-
 //TemplateID:  d-20c6e30370374c6ca63b6f0179ccf14a
-import sendgrid from "@sendgrid/mail";
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
 export async function POST(req: Request, res: Response) {
 
+  let transporter = nodemailer.createTransport({
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'wearsworks@gmail.com', // Add your username here
+      pass: 'AYcB4Sy65MJEDzsF', // Use your master password here
+    },
+  });
   try {
     if (!req.body) {
       return new NextResponse("Invalid request", { status: 400 });
@@ -55,7 +63,28 @@ export async function POST(req: Request, res: Response) {
         templateId: 'd-02f228bbf32e4eacac163c274f27c920',
       };
         try {
-          await sendgrid.send(mail);
+          let info = await transporter.sendMail({
+            from: '"WearsWorks 👻" <wearsworks@gmail.com>', // sender address
+              to: email, // list of receivers
+                subject: 'New Booking', // Subject line
+                text: 'Hello world?', // plain text body
+                //white background color for the email and black for the text
+                html: ` 
+                <body style="font-family: 'Inter', sans-serif; padding: 20px; background-color: #fff; color: #000;">
+                <h1 style="color: #000;">New Booking</h1>
+                <h2>Dear ${name},</h2>
+                <p>You have a new booking from ${data.name} at ${data.hour}:00-${parseInt(hour) + 1}:00</p>
+                <p>Message: ${data.message}</p>
+                <p>Share Meeting Link: ${data.shareMeetingLink}</p>
+
+
+      
+      
+                <p>Thank you for using our service!</p>
+      
+                <p>Bookers Team</p>
+                `
+              });
           console.log("email sent");
           
         } catch (error: any) {
